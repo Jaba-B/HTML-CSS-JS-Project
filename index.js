4ef9ff982007bf96b660e2035c703d7d811f3f8f
Jaba-B <jb.beroshvili@gmail.com>
2023-01-12 22:35:20 +0400

    add question-page with javascript functionality

--- index.js
+++ index.js
@@ -24,3 +24,37 @@ dot3.addEventListener('click', () => {
     dot1.style.backgroundColor = '#CBCED6';
     reviews.style.justifyContent = 'end';
 });
+
+
+const arrow1 = document.getElementById('question-arrow1');
+const response1 = document.getElementById('response1');
+
+const arrow2 = document.getElementById('question-arrow2');
+const response2 = document.getElementById('response2');
+
+const arrow3 = document.getElementById('question-arrow3');
+const response3 = document.getElementById('response3');
+
+const arrow4 = document.getElementById('question-arrow4');
+const response4 = document.getElementById('response4');
+
+
+arrow1.addEventListener('click', () => {
+    arrow1.classList.toggle("rotate");
+    response1.classList.toggle("visible");
+})
+
+arrow2.addEventListener('click', () => {
+    arrow2.classList.toggle("rotate");
+    response2.classList.toggle("visible");
+})
+
+arrow3.addEventListener('click', () => {
+    arrow3.classList.toggle("rotate");
+    response3.classList.toggle("visible");
+})
+
+arrow4.addEventListener('click', () => {
+    arrow4.classList.toggle("rotate");
+    response4.classList.toggle("visible");
+})
